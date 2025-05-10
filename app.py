f10b44c4753b8ad796624bf457e36ce308e9531c
kajuliaa <julia.kaidashova5@gmail.com>
2025-05-10 21:59:00 +0200

    added UI

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from models import db
+from flask import Flask, render_template, redirect, url_for
+from models import db, Room
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kaidashova:123456789@localhost:5432/student_rooms'
@@ -9,4 +9,12 @@ db.init_app(app)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    rooms = Room.query.all()
+    return render_template('index.html', rooms = rooms)
+@app.route('/book/<int:room_id>', methods=['POST'])
+def book(room_id):
+    room = Room.query.get_or_404(room_id)
+    if room.occupancy < room.capacity:
+        room.occupancy +=1
+        db.session.commit()
+    return redirect(url_for('home'))
