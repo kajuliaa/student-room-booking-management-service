b929017a5fe8b869d0512804a96b12cd62aeb15e
kajuliaa <julia.kaidashova5@gmail.com>
2025-05-12 16:01:10 +0200

    added overlapping logic

--- app.py
+++ app.py
@@ -18,8 +18,17 @@ def book():
     room_id = int(request.form['room_id'])
     start = datetime.fromisoformat(request.form['start'])
     end= datetime.fromisoformat(request.form['end'])
-
     
+    # 1. Check for overlapping bookings
+    overlapping = Booking.query.filter(
+        Booking.room_id == room_id,
+        Booking.end_time > start,   # Existing booking ends *after* requested start
+        Booking.start_time < end    # Existing booking starts *before* requested end
+    ).first()
+
+    if overlapping:
+        return f'This time slot is already booked for Room {room_id}!', 400
+
     
     # Create booking
     new_booking = Booking(room_id = room_id, start_time= start, end_time= end)
